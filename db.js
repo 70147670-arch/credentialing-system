9889a36934aedcfc3b514982face8f72eb2f611a
Marya <marya@gmail.com>
2026-04-13 19:45:00 +0500

    hardcode db fix

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  host: "monorail.proxy.rlwy.net",
+  user: "root",
+  password: "TOycBaqbbCtxWIqfNBukkFHJUcUMWdzq",
+  database: "railway",
+  port: 27375
 });
 
 db.connect((err) => {
