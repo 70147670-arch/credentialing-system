52546c8079560dfb92fd051e901dd2debd034c53
Marya <marya@gmail.com>
2026-04-13 18:54:53 +0500

    fix duplicate authRoutes + db

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
-const db = require("./db");
 const express = require("express");
 const cors = require("cors");
-const authRoutes = require("./routes/auth");
+
 const app = express();
-const requestRoutes = require("./routes/request"); 
+
+const authRoutes = require("./routes/auth");
+const requestRoutes = require("./routes/request");
 
 app.use(cors());
 app.use(express.json());
-const authRoutes = require("./routes/auth");
+
+// routes
 app.use("/api/auth", authRoutes);
 app.use("/api/request", requestRoutes);
-app.use("/api/auth", authRoutes);
+
+// test route
 app.get("/", (req, res) => {
   res.send("API is working 🚀");
-}); 
+});
+
+// uploads
 app.use("/uploads", express.static("uploads"));
+
+// start server
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
