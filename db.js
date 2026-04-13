cbbef2618bab7663c4f8ca9456ff64dc632e6f72
Marya <marya@gmail.com>
2026-04-13 19:04:22 +0500

    final pool fix

--- db.js
+++ db.js
@@ -1,19 +1,13 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10
 });
 
-db.connect(err => {
-  if (err) {
-    console.log("DB Error ❌", err);
-  } else {
-    console.log("MySQL Connected ✅");
-  }
-});
-
-module.exports = db;
+module.exports = pool;
